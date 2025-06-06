57c05c67f8ea17b45b9ef20c5ae7aa2a20421014
Sungmin Woo <119290712+sungmin-woo-devops@users.noreply.github.com>
2025-06-06 21:35:32 +0900

    Fix hover:shadow-glow class error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: [
+    'hover:shadow-glow',
+    'hover:shadow-glow-accent'
+  ],
   theme: {
     extend: {
       colors: {
